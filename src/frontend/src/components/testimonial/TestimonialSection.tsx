9f79c73ea5859d7be3598bf02bc72ef7ac02b26c
Gustavo Kophal <17kophal@gmail.com>
2024-09-03 13:13:18 -0300

    style: quick fix in the testimonial component style

--- src/frontend/src/components/testimonial/TestimonialSection.tsx
+++ src/frontend/src/components/testimonial/TestimonialSection.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './testimonialSection.css';
+import React from "react";
+import "./testimonialSection.css";
 
 const TestimonialSection: React.FC = () => {
   return (
     <div className="testimonial-section">
       <div className="testimonial-left">
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/trisog-e765d.appspot.com/o/images%2Fcoffee.jpg?alt=media&token=2d404efd-392e-4488-9789-6cb1a2561f60"
+          src="https://firebasestorage.googleapis.com/v0/b/trisog-e765d.appspot.com/o/images%2Ftestimonial.png?alt=media&token=f4c99c4a-93f8-47a4-839a-1f99624b7e6c"
           alt="Traveler"
           className="testimonial-image"
         />
@@ -14,7 +14,11 @@ const TestimonialSection: React.FC = () => {
       <div className="testimonial-right">
         <p className="testimonial-subtitle">Testimonials</p>
         <h2 className="testimonial-title">What Travelers Say</h2>
-        <p className="testimonial-quote">“The UI designs he crafted are top-notch, and the design system he integrated allows for straight forward fixes and bulk updates throughout almost every area of the app.”</p>
+        <p className="testimonial-quote">
+          “The UI designs he crafted are top-notch, and the design system he
+          integrated allows for straight forward fixes and bulk updates
+          throughout almost every area of the app.”
+        </p>
         <p className="testimonial-author">- By Molie Rosa, Photographer</p>
         <div className="testimonial-indicators">
           <span className="indicator active"></span>
